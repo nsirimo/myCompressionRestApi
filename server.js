deffed99cca292c036c826db116ed26feb4f8720
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2019-05-07 19:48:06 -0700

    Added starter code for myCompression done in a REST API way

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+async function myCompress(data) {
+    let current = data.charAt[0];
+    let currString = "";
+    let compressedString = "";
+
+    //scan through data (string)
+    for(let i = 0; i < data.length; i++) {
+        if(data.charAt(i) != current) {
+            const count = currString.length;
+            if(count < 16) {
+                compressedString += currString;
+            } else {
+                let compressed = "";
+                if(current == '0') {
+                    compressed += "-";
+                    compressed += count;
+                    compressed += "-";
+                    compressedString += compressed;
+                } else if(current == '1') {
+                    compressed += "+";
+                    compressed += count;
+                    compressed += "+";
+                    compressedString += compressed;
+                }
+            }
+            currString = data.charAt(i);
+            current = data.charAt(i);
+            counter = 1;
+        } else {
+            currString += data.charAt(i);
+        }
+    }
+    compressedString += "\n";
+
+    return compressedString;
+}
+
+app.post('/compressInput', function (req, res) {
+    console.log("this is it: " + req.body);
+  myCompress(req.body.inputString)
+  .then((compressedString) => {
+      res.send(compressedString).status(200);
+  });
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
