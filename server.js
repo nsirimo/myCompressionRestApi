6ae47d5c9865918bb59e99a897e352dc99b7e405
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2019-05-07 19:59:14 -0700

    Added default route

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.post('/compressInput', function (req, res) {
   });
 });
 
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
